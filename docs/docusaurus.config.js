36839e2e7799ac4ec8cfc31c2342835516022557
#Einswilli <wilfriedgoeh@gmail.com>
2026-04-03 20:42:58 +0000

    initialize docusaurus project.

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,111 @@
+// @ts-check
+import { themes as prismThemes } from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Ryx ORM',
+  tagline: 'Django-style Python ORM. Powered by Rust.',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://ryx.alldotpy.dev',
+  baseUrl: '/',
+
+  organizationName: 'AllDotPy',
+  projectName: 'Ryx',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          path: './doc',
+          routeBasePath: '/',
+          sidebarPath: './sidebars.js',
+          editUrl: 'https://github.com/AllDotPy/Ryx/tree/main/docs/doc/',
+          showLastUpdateTime: true,
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/social-card.png',
+      navbar: {
+        title: 'Ryx',
+        logo: {
+          alt: 'Ryx ORM Logo',
+          src: 'img/logo.svg',
+          width: 32,
+          height: 32,
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {
+            type: 'custom-search-bar',
+            position: 'right',
+          },
+          {
+            type: 'custom-github-stats',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/AllDotPy/Ryx/blob/main/CONTRIBUTING.md',
+            position: 'right',
+            label: 'Contributing',
+          },
+        ],
+      },
+      footer: undefined,
+      prism: {
+        theme: prismThemes.oneDark,
+        darkTheme: prismThemes.oneDark,
+        additionalLanguages: ['python', 'rust', 'bash', 'sql', 'toml'],
+        magicComments: [
+          {
+            className: 'theme-code-block-highlighted-line',
+            line: 'highlight-next-line',
+            block: { start: 'highlight-start', end: 'highlight-end' },
+          },
+        ],
+      },
+      colorMode: {
+        defaultMode: 'dark',
+        respectPrefersColorScheme: true,
+        disableSwitch: false,
+      },
+      docs: {
+        sidebar: {
+          hideable: true,
+          autoCollapseCategories: true,
+        },
+      },
+      algolia: undefined,
+    }),
+
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+};
+
+export default config;
---
+++ docs/sidebars.js
@@ -0,0 +1,114 @@
+const sidebars = {
+  tutorialSidebar: [
+    'intro',
+    {
+      type: 'category',
+      label: 'Getting Started',
+      link: { type: 'doc', id: 'getting-started/index' },
+      items: [
+        'getting-started/installation',
+        'getting-started/quick-start',
+        'getting-started/project-structure',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Core Concepts',
+      link: { type: 'doc', id: 'core-concepts/index' },
+      items: [
+        'core-concepts/models',
+        'core-concepts/managers-and-querysets',
+        'core-concepts/fields',
+        'core-concepts/migrations',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Querying',
+      link: { type: 'doc', id: 'querying/index' },
+      items: [
+        'querying/filtering',
+        'querying/q-objects',
+        'querying/ordering-and-pagination',
+        'querying/aggregations',
+        'querying/values-and-annotate',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Relationships',
+      link: { type: 'doc', id: 'relationships/index' },
+      items: [
+        'relationships/foreign-key',
+        'relationships/one-to-one',
+        'relationships/many-to-many',
+        'relationships/select-related',
+        'relationships/prefetch-related',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'CRUD',
+      link: { type: 'doc', id: 'crud/index' },
+      items: [
+        'crud/creating',
+        'crud/reading',
+        'crud/updating',
+        'crud/deleting',
+        'crud/bulk-operations',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Advanced',
+      link: { type: 'doc', id: 'advanced/index' },
+      items: [
+        'advanced/transactions',
+        'advanced/validation',
+        'advanced/signals',
+        'advanced/hooks',
+        'advanced/caching',
+        'advanced/custom-lookups',
+        'advanced/sync-async',
+        'advanced/raw-sql',
+        'advanced/cli',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Reference',
+      link: { type: 'doc', id: 'reference/index' },
+      items: [
+        'reference/api-reference',
+        'reference/field-reference',
+        'reference/lookup-reference',
+        'reference/exceptions',
+        'reference/signals-reference',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Internals',
+      link: { type: 'doc', id: 'internals/index' },
+      items: [
+        'internals/architecture',
+        'internals/rust-core',
+        'internals/query-compiler',
+        'internals/connection-pool',
+        'internals/type-conversion',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Cookbook',
+      link: { type: 'doc', id: 'cookbook/index' },
+      items: [
+        'cookbook/blog-tutorial',
+        'cookbook/testing',
+        'cookbook/deployment',
+      ],
+    },
+  ],
+};
+
+module.exports = sidebars;
